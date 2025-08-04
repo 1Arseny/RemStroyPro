c143deb35bffe8fa85818fa1b7b3b6b0e011e7f2
F1zge <arsenijbaulin@yandex.ru>
2025-08-04 22:29:47 +0300

    смена структуры первой секции - боттом на всю ширину - без правок боттома

--- src/app/sections/KrutorusAndCalc/KrutorusAndCalc.tsx
+++ src/app/sections/KrutorusAndCalc/KrutorusAndCalc.tsx
@@ -1,13 +1,13 @@
 'use client'
 import styles from './KrutorusAndCalc.module.css'
-import Calc from "@/app/sections/KrutorusAndCalc/Calc/Calc";
+// import Calc from "@/app/sections/KrutorusAndCalc/Calc/Calc";
 import Krutorus from "@/app/sections/KrutorusAndCalc/Krutorus/Krutorus";
 
 function KrutorusAndCalc() {
     return (
         <section className={styles.main}>
             <Krutorus/>
-            <section id='calculator'><Calc/></section>
+            {/* <section id='calculator'><Calc/></section> */}
         </section>
     );
 }
