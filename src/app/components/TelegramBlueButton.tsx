2277c88652fc5420397795e86a81324f28b24faf
F1zge <arsenijbaulin@yandex.ru>
2025-08-06 18:21:04 +0300

    добавил 1ю цель на тг а главной

--- src/app/components/TelegramBlueButton.tsx
+++ src/app/components/TelegramBlueButton.tsx
@@ -1,15 +1,25 @@
-'use client'
-import styles from './TelegramBlueButton.module.css'
-import tgBlueSvg from 'public/tgBlue.svg'
-import Image from "next/image";
-import Link from "next/link";
+'use client';
+import styles from './TelegramBlueButton.module.css';
+import tgBlueSvg from 'public/tgBlue.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useCallback } from 'react';
 
 function TelegramBlueButton() {
+    const handleClick = useCallback(() => {
+        if (typeof window !== 'undefined' && typeof window.ym === 'function') {
+            window.ym(103620695, 'reachGoal', 'TelegramBlueButton');
+        }
+    }, []);
+
     return (
-        <Link href="https://t.me/BuildConsultBot?start=BlueMain">
+        <Link
+            href="https://t.me/BuildConsultBot?start=BlueMain"
+            onClick={handleClick}
+        >
             <div className={styles.button}>
                 Telegram
-                <Image src={tgBlueSvg} alt={'tg'}/>
+                <Image src={tgBlueSvg} alt={'tg'} />
             </div>
         </Link>
     );
