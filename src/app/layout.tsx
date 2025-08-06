6387713ca131966d89166f605af8e89beabf8a3c
F1zge <arsenijbaulin@yandex.ru>
2025-08-06 17:55:03 +0300

    инициализация метрики

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,46 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ru" className={robotoFlex.className}>
-        <body id="__next">{children}</body>
-        </html>
+        <head>
+            {/* Яндекс.Метрика */}
+            <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function(m,e,t,r,i,k,a){
+                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                    }
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                    k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=103620695', 'ym');
+
+                ym(103620695, 'init', {
+                    ssr:true,
+                    webvisor:true,
+                    clickmap:true,
+                    ecommerce:"dataLayer",
+                    accurateTrackBounce:true,
+                    trackLinks:true
+                });
+                `,
+            }}
+            />
+        </head>
+        <body id="__next">
+            {/* <noscript> должен быть в body */}
+            <noscript>
+            <div>
+                <img
+                src="https://mc.yandex.ru/watch/103620695"
+                style={{ position: "absolute", left: "-9999px" }}
+                alt=""
+                />
+            </div>
+            </noscript>
+            {children}
+        </body>
+    </html>
     );
 }
